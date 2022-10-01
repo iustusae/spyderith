3400eebf91c60812a1f5b07b9fda19f6859f5194
Ayman Ougri <103166756+aougri@users.noreply.github.com>
2022-09-30 22:16:18 -0400

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.github.twitch4j:twitch4j:1.12.0")
+    implementation ("org.apache.logging.log4j:log4j-api:2.19.0")
+    implementation ("org.apache.logging.log4j:log4j-core:2.19.0")
 }
 
 tasks.getByName<Test>("test") {
